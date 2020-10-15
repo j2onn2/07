34cb923feaa3cf8dfdb4b7e37f594069fe828fb6
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-15 15:37:01 +0900

    .

--- main.c
+++ main.c
@@ -5,15 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i	;
-	
-	for (i=0 ; i<5; i++)
+	int i = 10	;
+	printf("함수 호출전 i=%d\n", i)	;
+	inc (i)	;
+	printf("함수 호출 후 i=%d\n", i)	;
+	return 0	;
+}
+	int inc(int counter)
 	{
-		int temp = 1	;
-		printf("temp = %d\n", temp)	;
-		temp ++	;
+		counter ++	;
+		return counter	;
 	}
 	
-	return 0	;
 
-}
