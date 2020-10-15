fa092b2e058f08539a86bd6d055d0a1f521bd941
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-15 15:30:14 +0900

    .

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int flag = 1	;
-	while ( flag !=0)
-	{	int y	;
-		y = 3	;
-		flag = 0	;
+	int i	;
 	
+	for (i=0 ; i<5; i++)
+	{
+		int temp = 1	;
+		printf("temp = %d\n", temp)	;
+		temp ++	;
 	}
-	int y	;
-	y=4	;
 	
+	return 0	;
 
 }
